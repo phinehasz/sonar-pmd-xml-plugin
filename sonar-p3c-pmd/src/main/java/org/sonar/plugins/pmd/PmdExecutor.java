58b94298520bb393e155ecbf44848dcf6de0a3db
phinehasz <zhhiyp@qq.com>
2018-11-19 22:35:24 +0800

    xml修复

--- sonar-p3c-pmd/src/main/java/org/sonar/plugins/pmd/PmdExecutor.java
+++ sonar-p3c-pmd/src/main/java/org/sonar/plugins/pmd/PmdExecutor.java
@@ -44,63 +44,65 @@ import java.util.Collection;
 import java.util.List;
 
 public class PmdExecutor implements BatchExtension {
-  private final FileSystem fs;
-  private final RulesProfile rulesProfile;
-  private final PmdProfileExporter pmdProfileExporter;
-  private final PmdConfiguration pmdConfiguration;
-  private final JavaResourceLocator javaResourceLocator;
-  private final Settings settings;
+	private final FileSystem fs;
+	private final RulesProfile rulesProfile;
+	private final PmdProfileExporter pmdProfileExporter;
+	private final PmdConfiguration pmdConfiguration;
+	private final JavaResourceLocator javaResourceLocator;
+	private final Settings settings;
 
-  public PmdExecutor(FileSystem fileSystem, RulesProfile rulesProfile, PmdProfileExporter pmdProfileExporter,
-                     PmdConfiguration pmdConfiguration, JavaResourceLocator javaResourceLocator, Settings settings) {
-    this.fs = fileSystem;
-    this.rulesProfile = rulesProfile;
-    this.pmdProfileExporter = pmdProfileExporter;
-    this.pmdConfiguration = pmdConfiguration;
-    this.javaResourceLocator = javaResourceLocator;
-    this.settings = settings;
-  }
+	public PmdExecutor(FileSystem fileSystem, RulesProfile rulesProfile, PmdProfileExporter pmdProfileExporter,
+					   PmdConfiguration pmdConfiguration, JavaResourceLocator javaResourceLocator, Settings settings) {
+		this.fs = fileSystem;
+		this.rulesProfile = rulesProfile;
+		this.pmdProfileExporter = pmdProfileExporter;
+		this.pmdConfiguration = pmdConfiguration;
+		this.javaResourceLocator = javaResourceLocator;
+		this.settings = settings;
+	}
 
-  public Report execute() {
-    TimeProfiler profiler = new TimeProfiler().start("Execute PMD " + PmdVersion.getVersion());
+	public Report execute() {
+		TimeProfiler profiler = new TimeProfiler().start("Execute PMD " + PmdVersion.getVersion());
 
-    ClassLoader initialClassLoader = Thread.currentThread().getContextClassLoader();
-    URLClassLoader classLoader = createClassloader();
-    try {
-      Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
+		ClassLoader initialClassLoader = Thread.currentThread().getContextClassLoader();
+		URLClassLoader classLoader = createClassloader();
+		try {
+			Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
 
-      return executePmd(classLoader);
-    } finally {
-      Closeables.closeQuietly(classLoader);
-      Thread.currentThread().setContextClassLoader(initialClassLoader);
-      profiler.stop();
-    }
-  }
+			return executePmd(classLoader);
+		} finally {
+			Closeables.closeQuietly(classLoader);
+			Thread.currentThread().setContextClassLoader(initialClassLoader);
+			profiler.stop();
+		}
+	}
 
-  private Report executePmd(URLClassLoader classLoader) {
-    Report report = new Report();
+	private Report executePmd(URLClassLoader classLoader) {
+		Report report = new Report();
 
-    RuleContext context = new RuleContext();
-    context.setReport(report);
+		RuleContext context = new RuleContext();
+		context.setReport(report);
 
-    PmdTemplate pmdFactory = createPmdTemplate(classLoader);
-    executeRules(pmdFactory, context, javaFiles(Type.MAIN), PmdConstants.REPOSITORY_KEY);
-    executeRules(pmdFactory, context, javaFiles(Type.TEST), PmdConstants.TEST_REPOSITORY_KEY);
+		PmdTemplate pmdFactory = createPmdTemplate(classLoader);
+		executeRules(pmdFactory, context, javaFiles(Type.MAIN), PmdConstants.REPOSITORY_KEY);
+		executeRules(pmdFactory, context, javaFiles(Type.TEST), PmdConstants.TEST_REPOSITORY_KEY);
 
-    context.setLanguageVersion(new XmlLanguageModule().getVersion(""));
-    executeRules(pmdFactory, context, xmlFiles(Type.MAIN), PmdConstants.XML_REPOSITORY_KEY);
+		RuleContext xmlContext = new RuleContext();
+		xmlContext.setReport(report);
+		xmlContext.setLanguageVersion(new XmlLanguageModule().getVersion(""));
+		executeXmlRules(pmdFactory, xmlContext, xmlFiles(Type.MAIN), PmdConstants.XML_REPOSITORY_KEY);
 
-    pmdConfiguration.dumpXmlReport(report);
+		pmdConfiguration.dumpXmlReport(report);
 
-    return report;
-  }
+		return report;
+	}
 
-  public Iterable<File> javaFiles(Type fileType) {
-    FilePredicates predicates = fs.predicates();
-    return fs.files(predicates.and(
-      predicates.hasLanguage(Java.KEY),
-      predicates.hasType(fileType)));
-  }
+	public Iterable<File> javaFiles(Type fileType) {
+		FilePredicates predicates = fs.predicates();
+		return fs.files(predicates.and(
+				predicates.hasLanguage(Java.KEY),
+				predicates.hasType(fileType)));
+	}
 
 	public Iterable<File> xmlFiles(Type fileType) {
 		FilePredicates predicates = fs.predicates();
@@ -109,61 +111,86 @@ public class PmdExecutor implements BatchExtension {
 				predicates.hasType(fileType)));
 	}
 
-  public void executeRules(PmdTemplate pmdFactory, RuleContext ruleContext, Iterable<File> files, String repositoryKey) {
-    if (Iterables.isEmpty(files)) {
-      // Nothing to analyze
-      return;
-    }
+	public void executeRules(PmdTemplate pmdFactory, RuleContext ruleContext, Iterable<File> files, String repositoryKey) {
+		if (Iterables.isEmpty(files)) {
+			// Nothing to analyze
+			return;
+		}
 
-    RuleSets rulesets = createRulesets(repositoryKey);
-    if (rulesets.getAllRules().isEmpty()) {
-      // No rule
-      return;
-    }
+		RuleSets rulesets = createRulesets(repositoryKey);
+		if (rulesets.getAllRules().isEmpty()) {
+			// No rule
+			return;
+		}
 
-    rulesets.start(ruleContext);
+		rulesets.start(ruleContext);
 
-    for (File file : files) {
-      pmdFactory.process(file, rulesets, ruleContext);
-    }
+		for (File file : files) {
+			if (file.getAbsolutePath().endsWith(".java")) {
+				pmdFactory.process(file, rulesets, ruleContext);
+			}
+		}
 
-    rulesets.end(ruleContext);
-  }
+		rulesets.end(ruleContext);
+	}
 
-  private RuleSets createRulesets(String repositoryKey) {
-    String rulesXml = pmdProfileExporter.exportProfile(repositoryKey, rulesProfile);
-    File ruleSetFile = pmdConfiguration.dumpXmlRuleSet(repositoryKey, rulesXml);
-    String ruleSetFilePath = ruleSetFile.getAbsolutePath();
-    RuleSetFactory ruleSetFactory = new RuleSetFactory();
-    try {
-      RuleSet ruleSet = ruleSetFactory.createRuleSet(ruleSetFilePath);
-      return new RuleSets(ruleSet);
-    } catch (RuleSetNotFoundException e) {
-      throw new IllegalStateException(e);
-    }
-  }
+	public void executeXmlRules(PmdTemplate pmdFactory, RuleContext ruleContext, Iterable<File> files, String repositoryKey) {
+		if (Iterables.isEmpty(files)) {
+			// Nothing to analyze
+			return;
+		}
 
-  @VisibleForTesting
-  PmdTemplate createPmdTemplate(URLClassLoader classLoader) {
-    return PmdTemplate.create(getSourceVersion(), classLoader, fs.encoding());
-  }
+		RuleSets rulesets = createRulesets(repositoryKey);
+		if (rulesets.getAllRules().isEmpty()) {
+			// No rule
+			return;
+		}
 
-  private URLClassLoader createClassloader() {
-    Collection<File> classpathElements = javaResourceLocator.classpath();
-    List<URL> urls = Lists.newArrayList();
-    for (File file : classpathElements) {
-      try {
-        urls.add(file.toURI().toURL());
-      } catch (MalformedURLException e) {
-        throw new IllegalStateException("Fail to create the project classloader. Classpath element is invalid: " + file, e);
-      }
-    }
-    return new URLClassLoader(urls.toArray(new URL[urls.size()]), null);
-  }
+		rulesets.start(ruleContext);
 
-  private String getSourceVersion() {
-    String javaVersion = settings.getString(PmdConstants.JAVA_SOURCE_VERSION);
-    return javaVersion != null ? javaVersion : PmdConstants.JAVA_SOURCE_VERSION_DEFAULT_VALUE;
-  }
+		for (File file : files) {
+			if (file.getAbsolutePath().endsWith(".xml")) {
+				pmdFactory.process(file, rulesets, ruleContext);
+			}
+		}
+
+		rulesets.end(ruleContext);
+	}
+
+	private RuleSets createRulesets(String repositoryKey) {
+		String rulesXml = pmdProfileExporter.exportProfile(repositoryKey, rulesProfile);
+		File ruleSetFile = pmdConfiguration.dumpXmlRuleSet(repositoryKey, rulesXml);
+		String ruleSetFilePath = ruleSetFile.getAbsolutePath();
+		RuleSetFactory ruleSetFactory = new RuleSetFactory();
+		try {
+			RuleSet ruleSet = ruleSetFactory.createRuleSet(ruleSetFilePath);
+			return new RuleSets(ruleSet);
+		} catch (RuleSetNotFoundException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+
+	@VisibleForTesting
+	PmdTemplate createPmdTemplate(URLClassLoader classLoader) {
+		return PmdTemplate.create(getSourceVersion(), classLoader, fs.encoding());
+	}
+
+	private URLClassLoader createClassloader() {
+		Collection<File> classpathElements = javaResourceLocator.classpath();
+		List<URL> urls = Lists.newArrayList();
+		for (File file : classpathElements) {
+			try {
+				urls.add(file.toURI().toURL());
+			} catch (MalformedURLException e) {
+				throw new IllegalStateException("Fail to create the project classloader. Classpath element is invalid: " + file, e);
+			}
+		}
+		return new URLClassLoader(urls.toArray(new URL[urls.size()]), null);
+	}
+
+	private String getSourceVersion() {
+		String javaVersion = settings.getString(PmdConstants.JAVA_SOURCE_VERSION);
+		return javaVersion != null ? javaVersion : PmdConstants.JAVA_SOURCE_VERSION_DEFAULT_VALUE;
+	}
 
 }
